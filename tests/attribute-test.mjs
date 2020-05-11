7f85384987010e3157c04bef070766e562db10dd
Markus Felten <Markus.Felten@gmx.de>
2020-05-12 00:30:04 +0200

    test: wip

--- tests/attribute-test.mjs
+++ tests/attribute-test.mjs
@@ -1,20 +1,13 @@
 import test from "ava";
 import { prepareBackend } from "./helpers/util.mjs";
 
-test("query", async t => {
-  const {
-    recordingNamespace,
-    backend,
-    writer,
-    a1,
-    s1
-  } = await prepareBackend({
+test("type", async t => {
+  const { recordingNamespace, backend, writer, a1, s1 } = await prepareBackend({
     a: 1,
     s: 1
   });
 
   ///writer.setTriple([s1, a1, 12], true);
 
-  backend.declareType('t1');
+  t.is(backend.declareType("t1"), "");
 });
-
