e56aa1dbfe919c21fc00c99f564922d73d546062
Markus Felten <Markus.Felten@gmx.de>
2020-05-25 01:59:36 +0200

    fix: use link from query

--- src/ontology.mjs
+++ src/ontology.mjs
@@ -15,46 +15,11 @@ export function SymatemOntologyMixin(base) {
 
     declareType(ic, name) {
       const { A } = this.placeholders(ic.tmpNamespace, { A: name });
-      const result = this.link(ic, [
-        [A, this.symbolByName.isa, this.symbolByName.Type]
-      ]);
+      const result = this.link(
+        [[A, this.symbolByName.isa, this.symbolByName.Type]],
+        ic.recordingNamespace
+      );
       return result[0][0];
-    }
-
-    /**
-     * Creates triples with associated data.
-     * But only if there are no such triples already
-     */
-    link(ic, queries) {
-      if (queries.length === 0) {
-        return [];
-      }
-
-      const query = queries[0];
-
-      const isPlaceholder = query.map(s => this.isPlaceholder(s));
-      const mask = this.queryMasks[
-        isPlaceholder.map(f => (f ? "V" : "M")).join("")
-      ];
-
-      for (const r of this.queryTriples(mask, query)) {
-        return [r, ...this.link(ic, queries.slice(1))];
-      }
-
-      const triple = query.map((s, i) => {
-        if (isPlaceholder[i]) {
-          const data = this.getLiteralData(s);
-          s = this.createSymbol(ic.recordingNamespace);
-          if (data !== undefined) {
-            this.setData(s, data);
-          }
-        }
-        return s;
-      });
-
-      this.setTriple(triple, true);
-
-      return [triple, ...this.link(ic, queries.slice(1))];
     }
   };
 }
