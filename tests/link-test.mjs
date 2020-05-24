e3a1eeb05f78ed79b0e92309f20dc7a820b81484
Markus Felten <Markus.Felten@db.com>
2020-05-24 16:37:48 +0200

    test: wip

--- tests/link-test.mjs
+++ tests/link-test.mjs
@@ -1,17 +1,31 @@
 import test from "ava";
 import { prepareBackend } from "./helpers/util.mjs";
 
-test("simple link", async t => {
-  const { backend, ic, a1, s1 } = await prepareBackend({
-    a: 1,
-    s: 1
+test("link single symbol with data", async t => {
+  const { backend, ic, s1, s2 } = await prepareBackend({
+    s: 2
   });
 
   const { A } = backend.placeholders(ic.tmpNamespace, { A: "xyz" });
 
-  const result = backend.link(ic, [[s1, a1, A]]);
+  const result = backend.link(ic, [[s1, s2, A]]);
 
   t.is(result[0], s1);
-  t.is(result[1], a1);
+  t.is(result[1], s2);
   t.is(backend.getData(result[2]), "xyz");
 });
+
+test("link two symbols one data", async t => {
+  const { backend, ic, s1 } = await prepareBackend({
+    s: 1
+  });
+
+  const { A, B } = backend.placeholders(ic.tmpNamespace, { A: "xyz", B: undefined });
+
+  const result = backend.link(ic, [[s1, A, B]]);
+
+  t.is(result[0], s1);
+  t.is(backend.getData(result[1]), "xyz");
+});
+
+
